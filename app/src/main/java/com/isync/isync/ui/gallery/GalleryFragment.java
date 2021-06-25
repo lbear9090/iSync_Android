a442dbddc557f7e88d4eb7814c7662d1601c4a86
Lucky <lbear9090@hotmail.com>
2021-06-25 04:45:57 -0700

    update

--- app/src/main/java/com/isync/isync/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/isync/isync/ui/gallery/GalleryFragment.java
@@ -28,13 +28,13 @@ public class GalleryFragment extends Fragment {
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+//        final TextView textView = binding.textGallery;
+//        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
     }
 
